92f6c6fabc31d005e8d3234c3ab8277a01854e5b
Arvydas Dudavicius <arvydas.dudavicius@gmail.com>
2019-02-19 12:25:41 +0200

    Initial Commit

---
+++ PayseraMokejimaiSDK/PayseraMokejimaiSDK.h
@@ -0,0 +1,19 @@
+//
+//  PayseraMokejimaiSDK.h
+//  PayseraMokejimaiSDK
+//
+//  Created by Arvydas Dudavicius on 19/02/2019.
+//  Copyright © 2019 Arvydas Dudavicius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayseraMokejimaiSDK.
+FOUNDATION_EXPORT double PayseraMokejimaiSDKVersionNumber;
+
+//! Project version string for PayseraMokejimaiSDK.
+FOUNDATION_EXPORT const unsigned char PayseraMokejimaiSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayseraMokejimaiSDK/PublicHeader.h>
+
+
